c53e618be937d725a4e86ad1a986efcb2f018f58
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2022-06-01 23:35:15 +0300

    arr + obj

---
+++ 2 - JS - Arr+Obj/index.js
@@ -0,0 +1,32 @@
+// const obj = { //key: "value"/value
+//     name: "Hanna",
+//     age: 18,
+//     getAge: function () {
+//         // function getWhat() {
+//         //     return this.age
+//         // }
+
+//         // getWhat()
+
+//         return this.age
+//     }
+// }
+
+// console.log(obj)
+// console.log(obj.name)
+// console.log(obj.getAge())
+
+// const arr = [{ sym: "aa", number: 5 }, { sym: "bb", number: 6 }, { sym: "cc", number: 7 }]
+
+// const arrNew = arr.map(i => {
+//     return i.sym + "a" //{ sym: "aa" }a
+// })
+
+// console.log(arr)
+// console.log(arrNew)
+
+// console.log(arr[0].number)
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
